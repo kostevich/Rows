6f6d9d6ea9f87f087c1f5cf2ec38bdaaba3825f0
Irina Dub <109979502+kostevich@users.noreply.github.com>
2024-04-28 18:36:47 +0300

    New: managing rows.

--- Manager.py
+++ Manager.py
@@ -2,33 +2,69 @@
 # >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
 #==========================================================================================#
 
+from Row import Row
 import os
 
-#==========================================================================================#
-# >>>>> МЕНЕДЖЕР <<<<< #
-#==========================================================================================#
-    
 class Manager():
     
-    #==========================================================================================#
-    # >>>>> КОНСТРУКТОР <<<<< #
-    #==========================================================================================#
-    
     def __init__(self):
-        pass
 
-    def GetFreeID(self):
+        # Словарь рядов.
+        self.__Rows = dict()
+
+        # Получение словаря рядов.
+        self.__GetDictRows()
+
+    def __GetDictRows(self):
+        """ Запись словаря, где ключ - ID; значение - объект Rows."""
+
+        # Получение списка файлов json.
+        RowsID = self.__GetRowsID()
+
+        # Запись словаря значениями ID.
+        for ID in RowsID:
+            self.__Rows[ID] = Row(ID)
+
+    def __GetFiles(self) -> list[str]:
 
         # Получение списка файлов в директории.
         Files = os.listdir("Data")
         # Фильтрация только файлов формата JSON.
         Files = list(filter(lambda List: List.endswith(".json"), Files))
-        
+
+        return Files
+
+    def __GetRowsID(self) -> list[int]:
+
+        # Получение списка файлов json.
+        Files = self.__GetFiles()
+
+        # Список ID.
+        RowsID = list()
+
+        # Для каждого файла получить ID.
+        for File in Files: RowsID.append(int(File.replace(".json", "")))
+
+        return RowsID
+
+    def CreateRow(self):
+
+        # Получение свободного ID.
+        ID = self.__GetFreeID()
+
+        # Запись ID.
+        self.__Rows[ID] = Row(ID)
+
+    def __GetFreeID(self):
+
+        # Получение списка ID.
+        RowsID = self.__GetRowsID()
+
+        # Получение списка файлов json.
+        Files = self.__GetFiles()
+
+        # Если файлы существуют.
         if Files != []:
-            # Список ID.
-            RowsID = list()
-            # Для каждого файла получить ID.
-            for File in Files: RowsID.append(int(File.replace(".json", "")))
 
             # ID нового ряда.
             ID = None 
@@ -49,3 +85,12 @@ class Manager():
         else: ID = 1
 
         return ID
+    
+    def RemoveRow(self, ID):
+
+		# Сохранение ID файла, который требуется удалить.
+        self.ID = ID
+
+        # Удаление файла json.
+        os.remove(f"Data/{self.ID}.json")
+ 
