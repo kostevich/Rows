c32fc9bf1589d5c578b54c865c4651375633c892
Irina Dub <109979502+kostevich@users.noreply.github.com>
2024-05-30 21:37:14 +0300

    Update Interpretator.py

--- Interpretator.py
+++ Interpretator.py
@@ -3,36 +3,30 @@ from dublib.Terminalyzer import *
 class Interpretator():
     def __init__(self) -> None:
 
-        self.__CommandsList = list()
         self.__AddCommands()
-
-    def __HandlerCommandLine(self, CommandLine):
-        
-        if CommandLine in self.__CommandsList: self.__HandlerCommands()
-        else: print("Command not found")
-
-    def __HandlerCommands(self):
-        
-        print("Processing the command")
 
     def __AddCommands(self):
 
+        CommandsList = list()
+
         Com = Command("createrow")
-        self.__CommandsList.append(Com)
+        CommandsList.append(Com)
 
         Com = Command("deleterow")
         Com.add_argument(ArgumentsTypes.Number, important = True)
-        self.__CommandsList.append(Com)
+        CommandsList.append(Com)
 
         Com = Command("listrows")
-        self.__CommandsList.append(Com)
+        CommandsList.append(Com)
 
-        return self.__CommandsList
+        return CommandsList
 
     def Run(self):
         while True:
-            CommandLine = input("Rows: ")
             
+            CommandLine = input("Rows: ")
+            ParsedCommand = Terminalyzer(CommandLine.split(" ")).check_commands(self.__AddCommands())
+
             if CommandLine != "": self.__HandlerCommandLine(CommandLine)
             else: CommandLine = input("Rows: ")
 
